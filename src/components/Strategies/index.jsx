80fe26999dc62f2d9a1e0a9b6cd7797e0949d379
Ahror02 <107356438+Ahror02@users.noreply.github.com>
2023-05-21 23:43:58 -0700

    Strategies changed

--- src/components/Strategies/index.jsx
+++ src/components/Strategies/index.jsx
@@ -24,7 +24,7 @@ const  Strategies = () => {
               <h2 className='site-strategies__title'>Contents Strategies</h2>
               <p className='site-strategies__subtitle'>We focus on ergonomics and meeting you where you work.Its only a keystroke away. </p>
               <div className="site-strategies__cards">
-                  <div className = "site-strategies__card "
+                  <div className = "site-strategies__card hover1"
                   data-aos = "fade-right"
                   data-aos-duration = "3000" >
                       <img className='site-strategies__card--img'
@@ -36,8 +36,9 @@ const  Strategies = () => {
                       <p className='site-strategies__card--text'>By <span className='site-strategies__card--text--span' > Wahid Ari </span>   |   03 March 2019 </p>
                       <h3 className='visually-hidden'>strategies card</h3>
                       <h4 className='site-strategies__card--title'>Increasing Prosperity With Positive Thinking</h4>   
+                      <span className='site-strategies__card--dots--dot1'></span>
                   </div>
-                   <div className="site-strategies__card"
+                   <div className="site-strategies__card hover2"
                       data-aos = "fade-up"
                       data-aos-duration = "3000" >
                       <img className='site-strategies__card--img'
@@ -51,8 +52,10 @@ const  Strategies = () => {
                       <p className='site-strategies__card--text'>By <span  className='site-strategies__card--text--span'>Wahid Ari</span> |  03 March 2019</p>
                       <h3 className='visually-hidden'>strategies card</h3>
                       <h4 className='site-strategies__card--title'>Motivation Is The First Step To Success</h4>   
+                      <span className='site-strategies__card--dots--dot2'></span>
+
                   </div>
-                   <div className = "site-strategies__card"
+                   <div className = "site-strategies__card hover3"
                    data-aos = "fade-left"
                   data-aos-duration = "3000" >
                       <img className='site-strategies__card--img'
@@ -68,7 +71,10 @@ const  Strategies = () => {
                       <p className='site-strategies__card--text'>By <span  className='site-strategies__card--text--span'>Wahid Ari</span> |  03 March 2019</p>
                       <h3 className='visually-hidden'>strategies card</h3>
                       <h4 className='site-strategies__card--title'>Success Steps For Your Personal Or Business</h4>   
+                      <span className='site-strategies__card--dots--dot3'></span>
+
                   </div>
+                  
               </div>
             </div>
          </div>
